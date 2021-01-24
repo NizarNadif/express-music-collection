534b97a1c7ef95a84cc8fb936ccb838027fd2210
Nizar Nadif <56386611+NizarNadif@users.noreply.github.com>
2021-01-24 16:15:16 +0100

    added functions to get data

--- scripts.js
+++ scripts.js
@@ -1,25 +1,41 @@
-export function getAllVinyls() {
+module.exports = {
+	getAllVinyls,
+	getAllAuthors,
+	getAllGenres,
+
+	// get di un dato singolo
+	getVinyl,
+	getAuthor,
+	getGenre,
+};
+
+const fs = require("fs");
+
+function getAllVinyls() {
 	const data = JSON.parse(fs.readFileSync("./vinyls.json").toString());
 	return data;
 }
 function getAllAuthors() {
-	const data = JSON.parse(fs.readFileSync("./vinyls.json").toString());
+	const data = JSON.parse(fs.readFileSync("./authors.json").toString());
 	return data;
 }
 function getAllGenres() {
-	const data = JSON.parse(fs.readFileSync("./vinyls.json").toString());
+	const data = JSON.parse(fs.readFileSync("./genres.json").toString());
 	return data;
 }
 
-function getVinyl() {
+function getVinyl(id) {
 	const data = JSON.parse(fs.readFileSync("./vinyls.json").toString());
-	return data;
+	const result = data.filter((vinyl) => vinyl.id == id)[0];
+	return result;
 }
-function getAuthor() {
+function getAuthor(id) {
 	const data = JSON.parse(fs.readFileSync("./vinyls.json").toString());
-	return data;
+	const result = data.filter((vinyl) => vinyl.autore == id);
+	return result;
 }
-function getGenre() {
+function getGenre(id) {
 	const data = JSON.parse(fs.readFileSync("./vinyls.json").toString());
-	return data;
+	const result = data.filter((vinyl) => vinyl.genere == id);
+	return result;
 }
